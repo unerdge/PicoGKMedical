d9d617709fde33ae264d191641f522e002b7b160
unerdge <2468910580@qq.com>
2026-01-06 14:43:02 +0800

    first commit

---
+++ BoneConstruct/Program.cs
@@ -0,0 +1,21 @@
+﻿using PicoGK;
+
+string strOutputFolder = "E:/desktop_files/workplace/vscode/C#(picoGK)/LEAP71/PicoGK_Examples-mainj/log";
+
+try
+{
+    
+    PicoGK.Library.Go(
+        0.2f,
+        Leap71.CoolCube.HelixHeatX.Task);
+    // PicoGK.Library.Go(
+    //     0.1f,
+    //     Practice.Task3,
+    //     strOutputFolder
+    //     );
+}
+catch (Exception e)
+{
+    Console.WriteLine("Failed to run Task.");
+    Console.WriteLine(e.ToString());
+}
